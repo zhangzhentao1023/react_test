098f0275bf5334ab01f4950d639d70668ab1a75a
t <666@qq.com>
2019-06-14 23:38:55 +0800

    login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {HashRouter,BrowserRouter,Route,Switch} from 'react-router-dom'
+import Login from './pages/login/Login'
+import Admin from './pages/admin/Admin'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+
+  render (){
+    return (
+        <HashRouter>
+          <Switch>
+            <Route path="/login" component={Login}></Route>
+            <Route path="/" component={Admin}></Route>
+          </Switch>
+        </HashRouter>
+
+    )
+  }
 }
 
-export default App;
+
+
+
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+import {Form,Icon,Input,Button} from 'antd'
+import logo from './images/logo.png'
+import './login.less'
+const Item = Form.Item
+
+export default class Login extends Component {
+
+    handleSubmit = (event) => {
+        // 阻止默认行为
+        event.preventDefault()
+        alert('提交')
+    }
+
+    render() {
+        return (
+            <div className="login">
+                <header className="login-header">
+                    <img src={logo} alt="logo"/>
+                    <h1>React项目：后台管理系统</h1>
+                </header>
+                <section className="login-content">
+                    <h2>用户登录</h2>
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Item>
+                            <Input
+                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                placeholder="用户名"
+                            />
+                        </Item>
+                        <Form.Item>
+                            <Input
+                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                type="password"
+                                placeholder="密码"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                登 录
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </section>
+            </div>
+        )
+    }
+}
